30b3fb5cef5ba9cc140d8c400ee09106332494a9
vcontreras <contrerastrece@gmail.com>
2021-05-18 20:04:09 -0500

    logica del juego funcionando

--- js/app.js
+++ js/app.js
@@ -1,19 +1,18 @@
-const pokemons = [1, 2, 3, 4, 5, 6, 7, 8];
-
-
 const drawCards = (allCards) => {
-  const $gameContainer=document.querySelector(".game-container");
-  const $fragment=document.createDocumentFragment();
-  const $template=document.getElementById("template").content;
+  const $gameContainer = document.querySelector(".game-container");
+  const $fragment = document.createDocumentFragment();
+  const $template = document.getElementById("template").content;
 
   let $clone_card;
 
-  allCards.forEach(cardN=>{
-    $template.querySelector("img").setAttribute("src",`assets/png/${cardN}.png`);
-
-    $clone_card=document.importNode($template,true);
+  allCards.forEach((cardN) => {
+    $template
+      .querySelector("img")
+      .setAttribute("src", `assets/png/${cardN}.png`);
+    $template.querySelector(".card").dataset.id = cardN;
+    $clone_card = document.importNode($template, true);
     $fragment.appendChild($clone_card);
-  })  
+  });
 
   $gameContainer.appendChild($fragment);
 };
@@ -21,8 +20,8 @@ const drawCards = (allCards) => {
 const getRandomNumber = (max = 150) => Math.floor(Math.random() * max + 1);
 
 const generatePokeCards = () => {
-  const currentCards = [] ;
-  while (currentCards.length < 4) {
+  const currentCards = [];
+  while (currentCards.length < 6) {
     let number = getRandomNumber();
 
     if (currentCards.includes(number)) {
@@ -33,20 +32,65 @@ const generatePokeCards = () => {
   }
 
   // obtener los cuatro pares
-  const allCards=[...currentCards,...currentCards].sort(()=>Math.random() - 0.5)
+  const allCards = [...currentCards, ...currentCards].sort(
+    () => Math.random() - 0.5
+  );
   // desordenar los elementos del array
   // allCards.sort(()=>Math.random() - 0.5)
 
-  currentCards.length < 4 ? generatePokeCards() : drawCards(allCards);
-  console.log(allCards)
+  currentCards.length < 6 ? generatePokeCards() : drawCards(allCards);
+  console.log(allCards);
 };
 generatePokeCards();
 
 const $gameContainer = document.querySelector(".game-container");
+let firstSelection = 0;
+let secondSelection = 0;
+
+const setCardSelected = (firstElementSelected, secondElementSelected) => {
+  if (firstElementSelected.dataset.id === secondElementSelected.dataset.id) {
+    console.log("WIN");
+    firstElementSelected.dataset.pokewin = "true";
+    secondElementSelected.dataset.pokewin = "true";
+  } else {
+    console.log("Error");
+
+    // despues de su transisión eliminar las clases cardShow
+    // a los elementos seleccionados
+    secondElementSelected.addEventListener(
+      "transitionend",
+      () => {
+        firstElementSelected.classList.remove("card-show");
+        secondElementSelected.classList.remove("card-show");
+      },
+      { once: true }
+    );
+  }
+  firstSelection = 0;
+  secondSelection = 0;
+};
 
 $gameContainer.addEventListener("click", (e) => {
-  console.log(e.target.parentNode);
-  if (e.target.parentNode.classList.contains("card")) {
+  // si el selector es una card y también contiene el dataset===false,
+  // con esto ultimo hacemos que ya no pueda coger datos del card ya volteada
+  if (
+    e.target.parentNode.classList.contains("card") &&
+    e.target.parentNode.dataset.pokewin === "false"
+  ) {
     e.target.parentNode.classList.add("card-show");
+
+    if (firstSelection === 0) {
+      firstSelection = e.target.parentNode;
+      console.log(firstSelection, "first Selection");
+    } else {
+      // condicionamos que el primer click no tenga que ser igual que el segundo click
+      // así logramos que el usuario no vuelva a selccionar la misma carta
+      if (firstSelection !== e.target.parentNode) {
+        secondSelection = e.target.parentNode;
+        console.log(secondSelection, "Second Selection");
+
+        setCardSelected(firstSelection, secondSelection);
+      }
+    }
   }
 });
