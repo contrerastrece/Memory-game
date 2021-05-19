a4a39584b62e3830acbe297275dbdb003a495140
vcontreras <contrerastrece@gmail.com>
2021-05-19 17:26:52 -0500

    Agregando Bonus y puntuación

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+const showAllCards = (allCards) => {
+  allCards.forEach((card) => {
+    card.classList.add("card-show");
+  });
+};
+const hideAllCards = (allCards) => {
+  allCards.forEach((card) => {
+    card.classList.remove("card-show");
+  });
+};
+
 const drawCards = (allCards) => {
   const $gameContainer = document.querySelector(".game-container");
   const $fragment = document.createDocumentFragment();
@@ -15,6 +26,15 @@ const drawCards = (allCards) => {
   });
 
   $gameContainer.appendChild($fragment);
+
+  const $allCards = document.querySelectorAll(".card");
+  // mostrando todas las cartas
+  setTimeout(() => {
+    showAllCards($allCards);
+  }, 1000);
+  setTimeout(() => {
+    hideAllCards($allCards);
+  }, 4000);
 };
 
 const getRandomNumber = (max = 150) => Math.floor(Math.random() * max + 1);
@@ -46,12 +66,22 @@ generatePokeCards();
 const $gameContainer = document.querySelector(".game-container");
 let firstSelection = 0;
 let secondSelection = 0;
+let fails=0;
+let bonus=1;
+let points=0;
+const $failsText=document.getElementById("fails");
+const $bonusText=document.getElementById("bonus");
+const $pointsText=document.getElementById("points");
 
 const setCardSelected = (firstElementSelected, secondElementSelected) => {
   if (firstElementSelected.dataset.id === secondElementSelected.dataset.id) {
     console.log("WIN");
     firstElementSelected.dataset.pokewin = "true";
     secondElementSelected.dataset.pokewin = "true";
+    $bonusText.textContent=`X${bonus}`;
+    points=points+10*bonus;
+    $pointsText.textContent=`${points}`;
+    bonus=bonus+1;
   } else {
     console.log("Error");
 
@@ -62,9 +92,17 @@ const setCardSelected = (firstElementSelected, secondElementSelected) => {
       () => {
         firstElementSelected.classList.remove("card-show");
         secondElementSelected.classList.remove("card-show");
+
+        // mostrando los errors
+        fails=fails+1;
+        $failsText.textContent=`${fails}`;
+        bonus=1;
+        $bonusText.textContent=`X${bonus}`;
       },
       { once: true }
     );
+
+    
   }
   firstSelection = 0;
   secondSelection = 0;
@@ -81,15 +119,18 @@ $gameContainer.addEventListener("click", (e) => {
 
     if (firstSelection === 0) {
       firstSelection = e.target.parentNode;
-      console.log(firstSelection, "first Selection");
+
+      // console.log(firstSelection, "first Selection");
     } else {
       // condicionamos que el primer click no tenga que ser igual que el segundo click
       // así logramos que el usuario no vuelva a selccionar la misma carta
       if (firstSelection !== e.target.parentNode) {
         secondSelection = e.target.parentNode;
-        console.log(secondSelection, "Second Selection");
 
-        setCardSelected(firstSelection, secondSelection);
+        // console.log(secondSelection, "Second Selection");
+
+        setCardSelected(firstSelection, secondSelection);       
+
       }
     }
   }
